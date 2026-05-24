363fcef0809d75c8c95a2fd799130d145d73ddde
Syaif05 <syaifullohrohmat05@gmail.com>
2026-05-24 08:56:19 +0700

    Fix Android build: align Kotlin compile target dynamically with Java compiler targetCompatibility using projectsEvaluated hook

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,25 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+gradle.projectsEvaluated {
+    subprojects {
+        val javaCompileTasks = tasks.withType<JavaCompile>()
+        val kotlinCompileTasks = tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>()
+        
+        var javaTarget: String? = null
+        javaCompileTasks.forEach { task ->
+            javaTarget = task.targetCompatibility
+        }
+        
+        if (javaTarget != null) {
+            val targetString = javaTarget!!
+            kotlinCompileTasks.forEach { task ->
+                task.kotlinOptions.jvmTarget = targetString
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
